d932578a2761b9246cd31235360bb65471410567
matt <mps5276@rit.edu>
2023-11-19 11:09:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Harry Potter API
+
+A JSON-based API for getting information on Harry Potter characters and spells. 
+
+Hosted at https://hp-api.herokuapp.com/ and https://hp-api.onrender.com/  
+(Heroku free hosting tier support ends on November 28, 2022, as announced [here](https://devcenter.heroku.com/changelog-items/2461))
+
+## Postman Collections
+Download the .json file, import it  into Postman and use the [Visualizer feature](https://learning.postman.com/docs/sending-requests/visualizer/#viewing-visualizations), to view the characters as cards.
+![image](https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/software_photos/001/604/818/datas/original.jpg)
+Based on the [submission](https://devpost.com/software/postman-visualization-harry-potter-api) for the DevPost hackathon: Visualize for the Prize.
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var app = express();
+var characters = require("./data/characters.json");
+var spells = require("./data/spells.json");
+
+app.use("/api", function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", function (req, res) {
+  res.sendfile("public/index.html");
+});
+
+app.get("/api/characters", function (req, res) {
+  res.json(characters);
+});
+
+app.get("/api/characters/students", function (req, res) {
+  res.json(characters.filter((character) => character.hogwartsStudent));
+});
+
+app.get("/api/characters/staff", function (req, res) {
+  res.json(characters.filter((character) => character.hogwartsStaff));
+});
+
+app.get("/api/characters/house/:house", function (req, res) {
+  var house = req.params.house.toLowerCase();
+  res.json(
+    characters.filter((character) => character.house.toLowerCase() === house)
+  );
+});
+
+app.get("/api/spells", function(req, res){
+  res.json(spells);
+});
+
+app.use(express.static("public"));
+
+app.set("port", (process.env.PORT || 8000));
+
+console.log("Port:", app.get("port"));
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
+});
